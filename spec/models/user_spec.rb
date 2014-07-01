08d344bc65a67e11141e0f2fd479683282552497
Brian Maddy <brian@brianmaddy.com>
2014-07-01 10:44:21 -0500

    switched to rspec3 style test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe User, :type => :model do
 
   it 'by default has the role of a registered user (after it is saved)' do
     user = FactoryGirl.build(:user)
-    user.registered?.should be_falsey
+    expect(user.registered?).to be_falsey
     user.save
-    user.registered?.should be_truthy
+    expect(user.registered?).to be_truthy
   end
 
 end
