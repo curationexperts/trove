420e1f8e3df1aaddd6a47bb586c7dc9e8722be4c
Justin Coyne <justin@curationexperts.com>
2014-07-02 09:11:43 -0500

    Got add to collections javascript working

---
+++ app/assets/javascripts/add_to_collections.js
@@ -0,0 +1,39 @@
+Blacklight.onLoad(function() {
+  $(".document").draggable({
+    helper: 'clone'
+  });
+
+  function addToPersonalCollection(event, ui) {
+    addToCollection(event, ui, 'personal_collections')
+  }
+
+  function addToCourseCollection(event, ui) {
+    addToCollection(event, ui, 'course_collections')
+  }
+
+
+  function addToCollection(event, ui, path) {
+    pid = ui.draggable.data('document-id');
+    collection_id = $(event.target).data('collection-id');
+    $.ajax({
+      type: "PATCH",
+      url: '/' + path + '/' + collection_id + '/append_to',
+      data: {pid: pid},
+      success: function(data, status){
+        console.log(data);
+        console.log(status);
+      }
+    });
+  }
+
+  $(".personal-collection-list li").droppable({
+    hoverClass: 'drop-target-hover',
+    tolerance: 'pointer',
+    drop: addToPersonalCollection
+  });
+  $(".course-collection-list li").droppable({
+    hoverClass: 'drop-target-hover',
+    tolerance: 'pointer',
+    drop: addToCourseCollection
+  });
+});
