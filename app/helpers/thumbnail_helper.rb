f0a987a96b7cb5f380f2095faef06e49855769d8
Brian Maddy <brian@brianmaddy.com>
2014-07-03 01:04:20 -0500

    made collections have folder thumbnails

--- app/helpers/thumbnail_helper.rb
+++ app/helpers/thumbnail_helper.rb
@@ -1,15 +1,23 @@
 module ThumbnailHelper
-  def thumbnail_tag(document, image_options)
-    case document['active_fedora_model_ssi']
-    when "TuftsImage"
+  # works with SolrDocuments and ActiveFedora models
+  def thumbnail_tag(document, image_options={})
+    if is_a(document, TuftsImage)
       image_tag download_path(document.id, datastream_id: 'Thumbnail.png')
+    elsif is_a(document, CuratedCollection)
+      image_tag '/assets/folder_thumbnail.png'
     else
       content_tag :span, '', class: 'canonical-image'
     end
-    # if document.representative.present?
-    #   image_tag download_path(document.representative, datastream_id: 'thumbnail'), alt: 'Thumbnail', class: "canonical-image"
-    # else
-    #   content_tag :span, '', class: 'canonical-image'
-    # end
+  end
+
+  private
+
+  # Checks to see if this SolrDocument or model inherits from a given class
+  def is_a(document, klass)
+    if document.is_a? SolrDocument
+      document['active_fedora_model_ssi'].constantize.ancestors.include? klass
+    else
+      document.is_a? klass
+    end
   end
 end
