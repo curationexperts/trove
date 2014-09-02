616b9d09cab845bf986ff2096f747aa490c8efc9
Justin Coyne <justin@curationexperts.com>
2014-09-02 11:42:25 -0500

    Check that FactoryGirl is present before configuring. It's not loaded in production

--- config/initializers/factory_girl.rb
+++ config/initializers/factory_girl.rb
@@ -1,2 +1,4 @@
 # Add the factories from tufts_models to the load path
-FactoryGirl.definition_file_paths << Pathname.new("#{TuftsModels::Engine.root.to_s}/spec/factories")
+if defined? FactoryGirl
+  FactoryGirl.definition_file_paths << Pathname.new("#{TuftsModels::Engine.root.to_s}/spec/factories")
+end
