d09da78fc21c54ba309e496c0be14f625c8d5850
Justin Coyne <justin@curationexperts.com>
2014-09-12 11:39:15 -0500

    Move more methods into the export writer

--- app/models/concerns/power_point.rb
+++ app/models/concerns/power_point.rb
@@ -6,37 +6,6 @@ module PowerPoint
 
   PPTX_DIR = File.join(Rails.root, 'tmp', 'exports')
 
-  SLIDE_WIDTH  = 720  # in pixels
-  SLIDE_HEIGHT = 540  # in pixels
-  SLIDE_ASPECT_RATIO = SLIDE_WIDTH.to_f / SLIDE_HEIGHT.to_f
-
-  # Calculate offset, width, & height of the image on the slide
-  def coordinates(image_path)
-    source_img_width, source_img_height = get_image_dimensions(image_path)
-    source_aspect_ratio = source_img_width.to_f / source_img_height.to_f
-
-    if source_aspect_ratio > SLIDE_ASPECT_RATIO
-      width = SLIDE_WIDTH
-      height = (width / source_aspect_ratio).to_i
-    else
-      height = SLIDE_HEIGHT
-      width = (height * source_aspect_ratio).to_i
-    end
-
-    { x: SLIDE_WIDTH / 2 - width / 2,
-      y: SLIDE_HEIGHT / 2 - height / 2,
-      cx: width,
-      cy: height
-    }
-  end
-
-  # TODO: Move this into tufts_models gem on the TuftsImage
-  # class.  It doesn't really belong here.
-  # Something like:  image.dimensions
-  def get_image_dimensions(image_path)
-    `identify -format %wx%h "#{image_path}"`.split('x').map(&:to_i)
-  end
-
   def export_dir
     timestamp = Time.now.strftime("%Y_%m_%d_%H%M%S")
     dir = File.join(PPTX_DIR, timestamp)
