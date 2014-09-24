e6c14a83b9abbc41c22bccf1b56c0172f459378a
Brian Maddy <brian@brianmaddy.com>
2014-09-24 13:38:49 -0500

    removed tests that is duplicated in tufts_models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 describe User do
 
-  it 'by default has the role of a registered user (after it is saved)' do
-    user = FactoryGirl.build(:user)
-    expect(user).to_not be_registered
-    user.save
-    expect(user).to be_registered
-  end
-
   describe "#personal_collection" do
     let(:user) { FactoryGirl.create(:user, email: "a%b+c++d@e.com") }
     let(:collection) { user.personal_collection(true) }
