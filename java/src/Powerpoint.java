9a3af73603c1dc612c30fec050b3b3418ab19fcc
Mark Bussey <mark@curationexperts.com>
2014-10-20 23:26:36 -0500

    Set font sizes dynamically for ppt export

--- java/src/Powerpoint.java
+++ java/src/Powerpoint.java
@@ -17,6 +17,7 @@ import org.apache.poi.xslf.usermodel.XSLFTextBox;
 import org.apache.poi.xslf.usermodel.XSLFTextParagraph;
 import org.apache.poi.xslf.usermodel.XSLFTextRun;
 import org.apache.poi.xslf.usermodel.XSLFTextShape;
+import org.apache.poi.xslf.usermodel.TextAlign;
 
 
 // For reference:
@@ -150,14 +151,20 @@ public class Powerpoint {
 
     // Replace placeholder text with our data
     XSLFTextShape[] placeholders = titleSlide.getPlaceholders();
-    placeholders[0].setText(title);
+    placeholders[0].clearText();
+	XSLFTextParagraph header = placeholders[0].addNewTextParagraph();
+    header.setLevel(0);
+    header.setBullet(false);
+	header.setTextAlign(TextAlign.LEFT);
+	XSLFTextRun t1 = header.addNewTextRun();	
+	t1.setText(maxText(title, 100));
+	t1.setBold(true);
+	t1.setFontSize(titleFontSize(title));
 
     placeholders[1].clearText();
     XSLFTextParagraph para = placeholders[1].addNewTextParagraph();
     para.setLevel(0);
     para.setBullet(true);
-    XSLFTextRun r1 = para.addNewTextRun();
-    r1.setFontSize(24);
 
     // Add '\r' to the Strings so they'll be separate bullet points
     StringBuilder desc = new StringBuilder();
@@ -168,7 +175,10 @@ public class Powerpoint {
         desc.append('\r');
       }
     }
-    r1.setText(desc.toString());
+	
+    XSLFTextRun r1 = para.addNewTextRun();
+	r1.setText(maxText( desc.toString(), 900 ));
+	r1.setFontSize(bodyFontSize( desc.toString() ));
 
     for(int j=2; j<placeholders.length; j++) {
       placeholders[j].clearText();
@@ -203,6 +213,32 @@ public class Powerpoint {
       }
     }
     return outputFileName;
+  }
+  
+  private static int bodyFontSize(String s) {
+    int size = s.length();
+	if      (size < 300) { return 28; }
+    else if (size < 500) { return 24; }
+    else                 { return 20; } // up to 900 characters in length works nicely
+  }
+
+  private static int titleFontSize(String s) {
+    int size = s.length();
+	if      (size < 45)  { return 40; }
+    else if (size < 75)  { return 32; }
+    else                 { return 28; } // up to 100 characters in length works nicely
+  }
+
+  private static String maxText(String s, int max_length) {
+	  int size = s.length();
+	  int best_space = s.substring(0, Math.min(size,max_length)).lastIndexOf(' ');
+	  
+	  // just return the string if it's short enough
+	  if (size < max_length) { return s; }  
+	  // otherwise, try to break on a space if there's one within the last 15 characters
+	  else if (best_space > max_length-15) { return s.substring(0, best_space) + "..."; } 
+	  // or just chomp it if there's no space near the end to break on
+	  else { return s.substring(0, max_length) + "..."; } 
   }
 
 }
