4d49496d17098ad3e3bc09f1ebece1f9f45e848e
Mike Korcynski <mkorcy@gmail.com>
2015-01-29 11:04:47 -0500

    adds help text for using trove

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_filter :authenticate_user!, only: [:about, :contact]
+  skip_before_filter :authenticate_user!, only: [:about, :contact, :help]
 
   def about
   end
 
   def contact
+  end
+
+  def help
   end
 end
